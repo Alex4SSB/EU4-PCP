67841967f5374db0ac3efb6f4e61a12e3a7f8131
Alex <alex4ssb@gmail.com>
2021-04-30 12:03:27 +0300

    Fixed const styles crashing tests with null reference

--- EU4-PCP/Models/PCP_Const.cs
+++ EU4-PCP/Models/PCP_Const.cs
@@ -13,8 +13,8 @@ namespace EU4_PCP
 		public static readonly string[] EUDF = {
 			"yyyy.M.dd", "yyyy.MM.dd", "yyyy.M.d", "yyyy.MM.d"
 		}; // EU Date Formats. The years 2 - 999 are interpreted falsely, and thus processed in the date parser
-		public static readonly Style GreenStyle = Application.Current.FindResource("GreenBackground") as Style;
-		public static readonly Style RedStyle = Application.Current.FindResource("RedBackground") as Style;
+		public static readonly Style GreenStyle = Application.Current?.FindResource("GreenBackground") as Style;
+		public static readonly Style RedStyle = Application.Current?.FindResource("RedBackground") as Style;
 		public static readonly Color RedBackground = Color.FromRgb(0xDE, 0x25, 0x25);
 		public static readonly Color GreenBackground = Color.FromRgb(0x7C, 0xB6, 0x1A);
 		public static readonly Color PurpleBackground = Color.FromRgb(0x6A, 0x33, 0x9E);
