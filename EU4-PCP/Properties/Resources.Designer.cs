126c48cb6d015e320092fe4fca27299fac4e2020
Alex <alex4ssb@gmail.com>
2022-06-23 19:12:08 +0300

    Updated all NuGet packages to latest stable versions

--- EU4-PCP/Properties/Resources.Designer.cs
+++ EU4-PCP/Properties/Resources.Designer.cs
@@ -70,7 +70,7 @@ namespace EU4_PCP.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 1.6.14.
+        ///   Looks up a localized string similar to 1.6.15.
         /// </summary>
         public static string AppVersion {
             get {
