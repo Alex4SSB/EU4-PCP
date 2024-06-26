7fb44017332ee872f1d5b9947c3d63be7522a1d4
Alex4SSB <Alex4SSB@gmail.com>
2024-06-26 23:23:00 +0300

    v1.6.18 release

--- EU4-PCP/Properties/Resources.Designer.cs
+++ EU4-PCP/Properties/Resources.Designer.cs
@@ -70,7 +70,7 @@ namespace EU4_PCP.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 1.6.17.
+        ///   Looks up a localized string similar to 1.6.18.
         /// </summary>
         public static string AppVersion {
             get {
